187330f318dbb7b293c45ec092b795c0e99d2c74
ctolon <cevat.batuhan.tolon@cern.ch>
2022-09-05 16:07:29 +0300

    o2-analysis-weakindices-decays in v0 task now optional, nightly version bumped up to date

--- NewAllWorkFlows/runV0selector.py
+++ NewAllWorkFlows/runV0selector.py
@@ -235,6 +235,7 @@ groupTaskAdders = parser.add_argument_group(title="Additional Task Adding Option
 groupTaskAdders.add_argument("--add_mc_conv", help="Add the converter from mcparticle to mcparticle+001 (Adds your workflow o2-analysis-mc-converter task)", action="store_true")
 groupTaskAdders.add_argument("--add_fdd_conv", help="Add the fdd converter (Adds your workflow o2-analysis-fdd-converter task)", action="store_true")
 groupTaskAdders.add_argument("--add_track_prop", help="Add track propagation to the innermost layer (TPC or ITS) (Adds your workflow o2-analysis-track-propagation task)", action="store_true")
+groupTaskAdders.add_argument("--add_weakdecay_ind", help="Add Converts V0 and cascade version 000 to 001 (Adds your workflow o2-analysis-weak-decay-indices task)", action="store_true")
 
 ##################
 # Interface Part #
@@ -368,7 +369,6 @@ if extrargs.FT0 != None:
 
 commonDeps = [
     "o2-analysis-timestamp",
-    "o2-analysis-weak-decay-indices",
     "o2-analysis-event-selection",
     "o2-analysis-multiplicity-table",
     "o2-analysis-trackselection",
@@ -622,7 +622,11 @@ if extrargs.add_fdd_conv:
 if extrargs.add_track_prop:
     commandToRun += " | o2-analysis-track-propagation --configuration json://" + updatedConfigFileName + " -b"
     logging.debug("o2-analysis-track-propagation added your workflow")
-
+    
+if extrargs.add_weakdecay_ind:
+    commandToRun += " | o2-analysis-weak-decay-indices --configuration json://" + updatedConfigFileName + " -b"
+    logging.debug("o2-analysis-weak-decay-indices added your workflow")
+    
 print("====================================================================================================================")
 logging.info("Command to run:")
 logging.info(commandToRun)
